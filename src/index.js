a666db5e8d620d33549ef6a4e284829a473d38a9
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2024-05-11 00:59:38 +0530

    Make the image carousel function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,62 @@
-/* eslint-disable import/no-import-module-exports */
-
 import 'normalize.css';
 import './style.css';
 
-console.log('Check whether everything is working or not.');
+class ImageCarousel {
+  constructor(previousSlideElem, nextSlideElem, reelElem, slideWidth) {
+    this.previousSlideElem = previousSlideElem;
+    this.nextSlideElem = nextSlideElem;
+    this.numberOfSlides =
+      Number(getComputedStyle(reelElem).width.replace('px', '')) / slideWidth;
+    this.reelElem = reelElem;
+    this.slideWidth = slideWidth;
+    this.currentSlideNumber = 1;
+  }
 
-/* if (process.env.NODE_ENV === 'development' && module.hot) {
-  module.hot.accept();
-} */
+  init() {
+    this.bound = {
+      showPreviousSlide: this.showPreviousSlide.bind(this),
+      showNextSlide: this.showNextSlide.bind(this),
+    };
+
+    this.previousSlideElem.addEventListener(
+      'click',
+      this.bound.showPreviousSlide,
+    );
+    this.nextSlideElem.addEventListener('click', this.bound.showNextSlide);
+  }
+
+  showPreviousSlide() {
+    if (this.currentSlideNumber === 1) {
+      this.currentSlideNumber = this.numberOfSlides;
+    } else {
+      this.currentSlideNumber -= 1;
+    }
+
+    this.render();
+  }
+
+  showNextSlide() {
+    if (this.currentSlideNumber === this.numberOfSlides) {
+      this.currentSlideNumber = 1;
+    } else {
+      this.currentSlideNumber += 1;
+    }
+    this.render();
+  }
+
+  render() {
+    this.reelElem.setAttribute(
+      'style',
+      `left: ${this.slideWidth * (1 - this.currentSlideNumber)}px;`,
+    );
+  }
+}
+
+const myCarousel = new ImageCarousel(
+  document.querySelector('.img-carousel__arrow--left'),
+  document.querySelector('.img-carousel__arrow--right'),
+  document.querySelector('.img-carousel__reel'),
+  600,
+);
+
+myCarousel.init();
